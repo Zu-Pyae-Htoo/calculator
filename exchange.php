9f6a76d71008e080e87918fab9a6c7165cceb3cf
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2024-02-10 00:09:30 +0630

    fixing UI

--- exchange.php
+++ exchange.php
@@ -13,100 +13,86 @@
                 </svg>
             </li>
             <li class="inline-flex items-center">
-                <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
-                    App Center
+                <a class="flex items-center text-sm font-semibold text-gray-700 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
+                    Exchange Calculator
                     <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="m9 18 6-6-6-6" />
                     </svg>
                 </a>
             </li>
-            <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200" aria-current="page">
-                Application
-            </li>
+
         </ol>
     </div>
     <hr class=" border border-gray-300 my-5">
 
-    <form action="./exchange-process.php" method="post">
-        <div class=" flex justify-between items-center gap-5">
-            <!-- From -->
-            <div class="relative flex-grow">
-                <select data-hs-select='{
-                    "placeholder": "Select option...",
+    <form action="">
+        <div class=" grid grid-cols-2 gap-5">
+            <div class=" col-span-full">
+                <label for="amount" class="block text-sm font-medium mb-2 dark:text-white">Ammount</label>
+                <input type="number" id="amount" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" name="amount">
+            </div>
+            <div class=" col-span-1">
+                <!-- Select -->
+                <div class="relative">
+                    <select data-hs-select='{
+                    "placeholder": "from currency...",
                     "toggleTag": "<button type=\"button\"></button>",
                     "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
                     "dropdownClasses": "mt-2 z-50 w-full max-h-[300px] p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-slate-900 dark:border-gray-700",
                     "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-200 dark:focus:bg-slate-800",
                     "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
                     }' class="hidden" name="from">
-                    <?php
-                    $content = file_get_contents("http://forex.cbm.gov.mm/api/latest");
-                    $currencies = json_decode($content)->rates;
+                        <option value="">from currency</option>
+                        <option>Name</option>
+                        <option>Email address</option>
+                        <option>Description</option>
+                        <option>User ID</option>
+                    </select>
 
-                    foreach ($currencies as $key => $value) :
-                    ?>
-                        <option value=<?= $value ?>>
-                            <?= $key ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-
-                <div class="absolute top-1/2 end-3 -translate-y-1/2">
-                    <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="m7 15 5 5 5-5" />
-                        <path d="m7 9 5-5 5 5" />
-                    </svg>
+                    <div class="absolute top-1/2 end-3 -translate-y-1/2">
+                        <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="m7 15 5 5 5-5" />
+                            <path d="m7 9 5-5 5 5" />
+                        </svg>
+                    </div>
                 </div>
+                <!-- End Select -->
             </div>
-            <!-- end from -->
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512 " class="h-5 w-5" stroke="none"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
-                <path d="M438.6 150.6c12.5-12.5 12.5-32.8 0-45.3l-96-96c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.7 96 32 96C14.3 96 0 110.3 0 128s14.3 32 32 32l306.7 0-41.4 41.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l96-96zm-333.3 352c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 416 416 416c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0 41.4-41.4c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-96 96c-12.5 12.5-12.5 32.8 0 45.3l96 96z" />
-            </svg>
-            <!-- to -->
-            <div class="relative flex-grow">
-                <select data-hs-select='{
-                    "placeholder": "Select option...",
+            <div class=" col-span-1">
+                <!-- Select -->
+                <div class="relative">
+                    <select data-hs-select='{
+                    "placeholder": "to currency...",
                     "toggleTag": "<button type=\"button\"></button>",
                     "toggleClasses": "hs-select-disabled:pointer-events-none hs-select-disabled:opacity-50 relative py-3 px-4 pe-9 flex text-nowrap w-full cursor-pointer bg-white border border-gray-200 rounded-lg text-start text-sm focus:border-blue-500 focus:ring-blue-500 before:absolute before:inset-0 before:z-[1] dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600",
                     "dropdownClasses": "mt-2 z-50 w-full max-h-[300px] p-1 space-y-0.5 bg-white border border-gray-200 rounded-lg overflow-hidden overflow-y-auto dark:bg-slate-900 dark:border-gray-700",
                     "optionClasses": "py-2 px-4 w-full text-sm text-gray-800 cursor-pointer hover:bg-gray-100 rounded-lg focus:outline-none focus:bg-gray-100 dark:bg-slate-900 dark:hover:bg-slate-800 dark:text-gray-200 dark:focus:bg-slate-800",
                     "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
-                    }' class="hidden" name="from">
-                    <?php
-                    $content = file_get_contents("http://forex.cbm.gov.mm/api/latest");
-                    $currencies = json_decode($content)->rates;
+                    }' class="hidden" name="to">
+                        <option value="">to currency</option>
+                        <option>Name</option>
+                        <option>Email address</option>
+                        <option>Description</option>
+                        <option>User ID</option>
+                    </select>
 
-                    foreach ($currencies as $key => $value) :
-                    ?>
-                        <option value=<?= $value ?>>
-                            <?= $key ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-
-                <div class="absolute top-1/2 end-3 -translate-y-1/2">
-                    <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                        <path d="m7 15 5 5 5-5" />
-                        <path d="m7 9 5-5 5 5" />
-                    </svg>
+                    <div class="absolute top-1/2 end-3 -translate-y-1/2">
+                        <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="m7 15 5 5 5-5" />
+                            <path d="m7 9 5-5 5 5" />
+                        </svg>
+                    </div>
                 </div>
+                <!-- End Select -->
             </div>
-            <!-- end to -->
-        </div>
 
-        <div class="mb-3 mt-3 flex justify-center items-center gap-3">
-            <label for="width" class="block  text-sm font-medium mb-2 dark:text-white">Amount</label>
-            <input type="number" name="home_width" id="width" class="py-3 px-4 block border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600">
-        </div>
-
-        <div class=" flex justify-between items-center gap-3 mt-6">
-            <button type="submit" name="cal_btn" class="w-2/3 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
+            <button type="submit" name="exchange_btn" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 col-span-full">
                 Calculate
             </button>
-            <button type="submit" name="cal_btn" class="w-1/3 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border-2 border-blue-600 border-transparent text-blue-500  hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">
-                Record List
-            </button>
         </div>
+
+
+
     </form>
 
 </section>
