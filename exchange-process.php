3e3f7b9cd42958d7797847341d334812c2656619
Zu-Pyae-Htoo <zupyaehtoo930@gmail.com>
2024-02-10 17:04:41 +0630

    calculator done

--- exchange-process.php
+++ exchange-process.php
@@ -1,3 +1,63 @@
 <?php
 
-$fCurrency = $_POST["from"];
+
+// print_r($_POST);
+
+$amount = $_POST["amount"];
+
+$from_currency_arr = explode("-", $_POST["from_currency"]);
+$from_currency_name = $from_currency_arr[0];
+$from_currency_rate = str_replace(",", "", $from_currency_arr[1]);
+
+$to_currency_arr = explode("-", $_POST["to_currency"]);
+$to_currency_name = $to_currency_arr[0];
+$to_currency_rate = str_replace(",", "", $to_currency_arr[1]);
+
+$mmk = $amount * $from_currency_rate;
+
+$to_currency = $mmk / $to_currency_rate;
+
+// echo $to_currency;
+
+?>
+
+<?php include("./template/header.php") ?>
+<?php include("./template/sidebar.php") ?>
+<section class=" bg-gray-200 p-10 rounded-lg">
+    <div class="mb-3">
+        <ol class="flex items-center whitespace-nowrap" aria-label="Breadcrumb">
+            <li class="inline-flex items-center">
+                <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
+                    Home
+                </a>
+                <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path d="m9 18 6-6-6-6" />
+                </svg>
+            </li>
+            <li class="inline-flex items-center">
+                <a href="./exchange.php" class=" flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
+                    Exchange Calculator
+                    <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <path d="m9 18 6-6-6-6" />
+                    </svg>
+                </a>
+            </li>
+            <li class="inline-flex items-center text-sm font-semibold text-gray-800 truncate dark:text-gray-200" aria-current="page">
+                Exchange Result
+            </li>
+        </ol>
+    </div>
+    <hr class=" border border-gray-200 my-5">
+    <p class=" text-5xl font-mono font-semibold flex justify-center items-center my-3">
+        <?= round($to_currency, 2) ?><?= $to_currency_name ?>
+    </p>
+    <div class=" flex gap-3">
+        <a href="./exchange.php" name="cal_btn" class=" py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 flex-grow">
+            Calculate Again
+        </a>
+        <a href="#" name="cal_btn" class=" py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg  border-2 border-blue-600 text-blue-600 hover:text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 flex-grow">
+            All Record
+        </a>
+    </div>
+</section>
+<?php include("./template/footer.php") ?>
--- exchange.php
+++ exchange.php
@@ -5,23 +5,17 @@
     <div class="mb-3">
         <ol class="flex items-center whitespace-nowrap" aria-label="Breadcrumb">
             <li class="inline-flex items-center">
-                <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
-                    href="#">
+                <a class="flex items-center text-sm text-gray-500 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
                     Home
                 </a>
-                <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600"
-                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <path d="m9 18 6-6-6-6" />
                 </svg>
             </li>
             <li class="inline-flex items-center">
-                <a class="flex items-center text-sm font-semibold text-gray-700 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500"
-                    href="#">
+                <a class="flex items-center text-sm font-semibold text-gray-700 hover:text-blue-600 focus:outline-none focus:text-blue-600 dark:focus:text-blue-500" href="#">
                     Exchange Calculator
-                    <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600"
-                        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <svg class="flex-shrink-0 mx-2 overflow-visible h-4 w-4 text-gray-400 dark:text-neutral-600" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                         <path d="m9 18 6-6-6-6" />
                     </svg>
                 </a>
@@ -32,18 +26,16 @@
     <hr class=" border border-gray-300 my-5">
 
     <?php
-        $content = file_get_contents("http://forex.cbm.gov.mm/api/latest");
-        $obj = json_decode($content)->rates;
-        
+    $content = file_get_contents("http://forex.cbm.gov.mm/api/latest");
+    $obj = json_decode($content)->rates;
+
     ?>
 
     <form action="./exchange-process.php" method="post">
         <div class=" grid grid-cols-2 gap-5">
             <div class=" col-span-full">
-                <label for="amount" class="block text-sm font-medium mb-2 dark:text-white">Ammount</label>
-                <input type="number" id="amount"
-                    class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600"
-                    name="amount">
+                <label for="amount" class="block text-sm font-medium mb-2 dark:text-white">Amount</label>
+                <input type="number" id="amount" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-slate-900 dark:border-gray-700 dark:text-gray-400 dark:focus:ring-gray-600" name="amount">
             </div>
             <div class=" col-span-1">
                 <!-- Select -->
@@ -57,15 +49,13 @@
                     "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
                     }' class="hidden" name="from_currency">
                         <option value="">From Currency</option>
-                        <?php foreach($obj as $key=>$value): ?>
-                        <option value="<?= $value ?>"><?= $key ?></option>
+                        <?php foreach ($obj as $key => $value) : ?>
+                            <option value="<?= $key ?>-<?= $value ?>"><?= $key ?></option>
                         <?php endforeach; ?>
                     </select>
 
                     <div class="absolute top-1/2 end-3 -translate-y-1/2">
-                        <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="m7 15 5 5 5-5" />
                             <path d="m7 9 5-5 5 5" />
                         </svg>
@@ -85,15 +75,13 @@
                     "optionTemplate": "<div class=\"flex justify-between items-center w-full\"><span data-title></span><span class=\"hidden hs-selected:block\"><svg class=\"flex-shrink-0 w-3.5 h-3.5 text-blue-600 dark:text-blue-500\" xmlns=\"http:.w3.org/2000/svg\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\"><polyline points=\"20 6 9 17 4 12\"/></svg></span></div>"
                     }' class="hidden" name="to_currency">
                         <option value="">to currency</option>
-                        <?php foreach($obj as $key=>$value): ?>
-                        <option value="<?= $value ?>"><?= $key ?></option>
+                        <?php foreach ($obj as $key => $value) : ?>
+                            <option value="<?= $key ?>-<?= $value ?>"><?= $key ?></option>
                         <?php endforeach; ?>
                     </select>
 
                     <div class="absolute top-1/2 end-3 -translate-y-1/2">
-                        <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500"
-                            xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                        <svg class="flex-shrink-0 w-3.5 h-3.5 text-gray-500 dark:text-gray-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                             <path d="m7 15 5 5 5-5" />
                             <path d="m7 9 5-5 5 5" />
                         </svg>
@@ -102,8 +90,7 @@
                 <!-- End Select -->
             </div>
 
-            <button type="submit"
-                class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 col-span-full">
+            <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600 col-span-full">
                 Calculate
             </button>
         </div>
